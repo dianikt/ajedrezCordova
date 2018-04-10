ef5f67bdc1a1b0cba8ad78b0daf3dc42fa3d10fa
super <dianikt7@gmail.com>
2018-04-10 19:58:16 +0200

    ajedrez primer

---
+++ www/js/index.js
@@ -0,0 +1,83 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+        //document.addEventListener("deviceready", this.detectoClick.bind(this), false);
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    },
+
+    pintaTablero: function() {
+        var letras = ["a","b","c","d","e","f","g","h"];
+        var tabla = $('#table_ajedrez');
+
+        for(var i=0;i<=8;i++){
+            for(var j=0;j<=8;j++){                
+                if(i == 0){
+                    tabla.append("<th>"+j+"</th>");
+                }else if(j == 0){
+                    tabla.append("<tr></tr>");
+                    tabla.append("<th>"+letras[i-1]+"</th>");
+                }else{
+                    if(j > 8){
+                        tabla.append("<tr></tr>");
+                    }else{
+                        if((i%2 == 0 && j%2 == 0)||(i%2 != 0 && j%2 != 0)){
+                            var fila = $("<td id="+i+j+" style='background-color:black'></td>");
+                           
+                         }else{                            
+                            var fila = $("<td id="+i+j+" style='background-color:white'></td>");                           
+                        }
+                    fila.click(movimiento);
+                    tabla.append(fila);
+                    }
+                }
+            }
+        }
+    },
+    
+};
+
+app.initialize();
+app.pintaTablero();
+
+function movimiento(event){
+    alert(event.target.id);
+}
+
